a979578bc324e9830be90a4460b23088d0692974
Arnelle Balane <arnellebalane@gmail.com>
2021-05-04 02:54:42 +0800

    Replace Header greeting with current day and date

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,22 +1,16 @@
 <script>
 import { createEventDispatcher } from 'svelte';
+import format from 'date-fns/format';
 import Button from '@components/Button.svelte';
 
 const dispatch = createEventDispatcher();
 
-const hour = new Date().getHours();
-const greeting = (() => {
-  if (hour < 12) {
-    return 'Good morning.';
-  } else if (hour < 18) {
-    return 'Good afternoon.';
-  }
-  return 'Good evening.';
-})();
+const date = new Date();
+const formattedDate = format(date, 'iiii, MMMM d');
 </script>
 
 <header class={$$props.class}>
-  <h1>{greeting}</h1>
+  <h1>{formattedDate}</h1>
 
   <Button primary on:click={() => dispatch('addtodo')}>Add Todo</Button>
   <Button text on:click={() => dispatch('removedone')}>Remove Done</Button>

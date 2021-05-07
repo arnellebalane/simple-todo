cec7fab6bbb747cb945f4b99dbad2e6db65fc190
Arnelle Balane <arnellebalane@gmail.com>
2021-05-08 03:04:15 +0800

    Implement dynamic theming based on settings and operating system

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import App from './App.svelte';
+import { settings } from '@stores/settings';
 import '@styles/index.css';
-import '@styles/themes/dark.css';
-import '@styles/themes/green.css';
+import '@styles/theme.css';
+
+settings.subscribe(({ theme }) => {
+  document.body.dataset.theme = theme;
+  document.body.dataset.color = 'green';
+});
 
 const app = new App({
   target: document.body,

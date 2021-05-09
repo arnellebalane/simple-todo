eae6704dae133e0dcbe441822c9a033daa7f1078
Arnelle Balane <arnellebalane@gmail.com>
2021-05-10 04:00:04 +0800

    Add refresh button in settings dialog to get new background image

--- src/components/SettingsFormBackgroundImageField.svelte
+++ src/components/SettingsFormBackgroundImageField.svelte
@@ -9,11 +9,27 @@ export let data = {
 
 const dispatch = createEventDispatcher();
 
-let sourceCache;
+let currentRequest;
+$: hasCurrentRequest = Boolean(currentRequest);
+
+const handleRefresh = async () => {
+  const { source, request } = settings.getBackgroundImage();
+  currentRequest = source;
+
+  try {
+    data.backgroundImage = await request;
+    currentRequest = null;
+    dispatch('change', data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 const handleBackgroundChange = async () => {
+  currentRequest?.cancel();
   if (data.background) {
     const { source, request } = settings.getBackgroundImage();
-    sourceCache = source;
+    currentRequest = source;
 
     try {
       data.backgroundImage = await request;
@@ -22,28 +38,59 @@ const handleBackgroundChange = async () => {
       data.background = false;
     }
   } else {
-    sourceCache?.cancel();
     delete data.backgroundImage;
   }
-  sourceCache = null;
+  currentRequest = null;
   dispatch('change', data);
 };
 </script>
 
 <div>
   <label for="background">Show background image</label>
+  {#if data.background}
+    <button type="button" disabled={hasCurrentRequest} on:click={handleRefresh}>Refresh</button>
+  {/if}
   <Switch name="background" bind:value={data.background} on:change={handleBackgroundChange} />
 </div>
 
 <style>
 div {
   display: flex;
-  justify-content: space-between;
   align-items: center;
+  gap: 8px;
 }
 
 label {
+  margin-right: auto;
   font-size: 1.8rem;
   font-weight: 700;
+}
+
+button {
+  width: 2.4rem;
+  height: 2.4rem;
+  border: none;
+  font-size: 0;
+  background: transparent url('./dist/assets/icons/refresh-dark.svg') center center no-repeat;
+  cursor: pointer;
+}
+
+button:disabled {
+  opacity: 0.5;
+  cursor: not-allowed;
+}
+
+:global(body[data-theme='DARK']) button {
+  background-image: url('./dist/assets/icons/refresh-light.svg');
+}
+
+@media (prefers-color-scheme: dark) {
+  button {
+    background-image: url('./dist/assets/icons/refresh-light.svg');
+  }
+
+  :global(body[data-theme='LIGHT']) button {
+    background-image: url('./dist/assets/icons/refresh-dark.svg');
+  }
 }
 </style>

97b605053d05f62f608915e76c4e72cd6a112d5d
Arnelle Balane <arnellebalane@gmail.com>
2021-05-19 22:21:41 +0800

    Add Netlify function to return changelogs after given version

---
+++ functions/get-version-changelog.js
@@ -0,0 +1,21 @@
+const changelogs = require('./data/changelogs.json');
+const semverGt = require('semver/functions/gt');
+
+exports.handler = async (event, context) => {
+  const version = event.queryStringParameters.version;
+  if (!version) {
+    return { statusCode: 400, body: 'Missing "version" query parameter.' };
+  }
+
+  const index = changelogs.findIndex((changelog) => semverGt(changelog.version, version));
+  const versionChangeLogs = changelogs.slice(index).map((changelog) => ({
+    ...changelog,
+    imageLight: process.env.URL + changelog.imageLight,
+    imageDark: process.env.URL + changelog.imageDark,
+  }));
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(versionChangeLogs),
+  };
+};

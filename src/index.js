d65cb1fdd52316527883809d7d1b5cf300f830e2
Arnelle Balane <arnellebalane@gmail.com>
2021-05-30 17:41:13 +0800

    Change imports to consistently not include .js extension

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import App from './App.svelte';
-import { initializeSentry } from '@lib/sentry.js';
-import { initializeTheme } from '@lib/theme.js';
-import { initializeKeyBindings } from '@lib/shortcuts.js';
+import { initializeSentry } from '@lib/sentry';
+import { initializeTheme } from '@lib/theme';
+import { initializeKeyBindings } from '@lib/shortcuts';
 import '@styles/index.css';
 import '@styles/theme.css';
 

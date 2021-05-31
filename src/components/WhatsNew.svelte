6de2f5197984cea19546844b64b5025fdd3d6f78
Arnelle Balane <arnellebalane@gmail.com>
2021-05-31 21:40:55 +0800

    Fix missing variable in WhatsNew component

--- src/components/WhatsNew.svelte
+++ src/components/WhatsNew.svelte
@@ -27,7 +27,7 @@ const handleNext = () => (index = Math.min(index + 1, screens.length - 1));
 
 <h2>What's new in Simple Todo</h2>
 
-{#each screens as screen}
+{#each screens as screen, i}
   <article class:active={i === index}>
     <h3>{screen.title}</h3>
     <picture class="Image--light">

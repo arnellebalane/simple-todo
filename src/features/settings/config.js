75d823ea5d1bca5fdd96fbd7efaaa2c7003c62d0
Arnelle Balane <arnellebalane@gmail.com>
2022-01-26 11:21:09 +0800

    Move misc settings code inside features/settings directory

--- src/features/settings/config.js
+++ src/features/settings/config.js
@@ -3,5 +3,13 @@ import * as BackgroundSettings from '@features/backgrounds/settings';
 import * as QuickLinksSettings from '@features/quick-links/settings';
 import * as TagsSettings from '@features/tags/settings';
 import * as ShortcutsSettings from '@features/shortcuts/settings';
+import * as MiscellaneousSettings from '@features/settings/settings';
 
-export const settingsTabs = [ThemeSettings, BackgroundSettings, QuickLinksSettings, TagsSettings, ShortcutsSettings];
+export const settingsTabs = [
+  ThemeSettings,
+  BackgroundSettings,
+  QuickLinksSettings,
+  TagsSettings,
+  ShortcutsSettings,
+  MiscellaneousSettings,
+];
---
+++ src/features/settings/settings/index.js
@@ -0,0 +1,5 @@
+import MiscellaneousSettings from './MiscellaneousSettings.svelte';
+
+export const id = 'MISCELLANEOUS';
+export const label = 'Miscellaneous';
+export const component = MiscellaneousSettings;

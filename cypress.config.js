9358eeec7fced542b57bed556de35dad9fe5f6f1
Arnelle Balane <arnellebalane@gmail.com>
2023-10-08 01:17:59 -0700

    Configure default viewport size in cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+    viewportWidth: 1920,
+    viewportHeight: 1080,
     component: {
         specPattern: 'src/**/*.spec.js',
         devServer: {

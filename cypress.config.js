1b5fac161038aa159d350e87c24e89d65cb87e71
Arnelle Balane <arnellebalane@gmail.com>
2024-02-03 16:31:36 +0800

    Configure cypress to read application URL from env variables

--- cypress.config.js
+++ cypress.config.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
@@ -16,7 +17,16 @@ module.exports = defineConfig({
         specPattern: 'cypress/integration/**/*.spec.js',
 
         setupNodeEvents(on, config) {
-            // implement node event listeners here
+            config.env = {
+                ...config.env,
+                APP_TESTING_ENDPOINT: process.env.APP_TESTING_ENDPOINT,
+            };
+
+            if (!config.env.APP_TESTING_ENDPOINT) {
+                throw new Error('APP_TESTING_ENDPOINT variable is not defined.');
+            }
+
+            return config;
         },
     },
 });

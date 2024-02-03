26c3f4261fc1f5b8712a5a5d2103dc4f1709a367
Arnelle Balane <arnellebalane@gmail.com>
2024-02-03 16:15:16 +0800

    Configure cypress for e2e  tests

--- cypress.config.js
+++ cypress.config.js
@@ -3,11 +3,20 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
     viewportWidth: 1920,
     viewportHeight: 1080,
+
     component: {
         specPattern: 'src/**/*.spec.js',
         devServer: {
             framework: 'svelte',
             bundler: 'vite',
+        },
+    },
+
+    e2e: {
+        specPattern: 'cypress/integration/**/*.spec.js',
+
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
         },
     },
 });

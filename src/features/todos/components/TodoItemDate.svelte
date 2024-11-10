0585842f7140e3e58f24bddaf65ee777e36fbc30
Arnelle Balane <arnellebalane@gmail.com>
2024-11-09 21:01:49 -0800

    Update Badge component and its usages to Svelte 5

--- src/features/todos/components/TodoItemDate.svelte
+++ src/features/todos/components/TodoItemDate.svelte
@@ -17,12 +17,15 @@ $: dateDisplay = (() => {
 })();
 </script>
 
-<Badge icon data-cy="todo-item-date">
+<Badge {icon} data-cy="todo-item-date">
+    {dateDisplay}
+</Badge>
+
+{#snippet icon()}
     <svg slot="icon" width="14" height="14" viewBox="0 0 24 24">
         <path
             fill="currentColor"
             d="M19 3H18V1H16V3H8V1H6V3H5C3.89 3 3 3.9 3 5V19C3 20.11 3.9 21 5 21H19C20.11 21 21 20.11 21 19V5C21 3.9 20.11 3 19 3M19 19H5V9H19V19M19 7H5V5H19V7Z"
         />
     </svg>
-    {dateDisplay}
-</Badge>
+{/snippet}
